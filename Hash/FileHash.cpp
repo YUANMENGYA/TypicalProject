2ededa1595d3ced3a443bb2ac1e9c7724ba8c1e7
GColom <33550861+GColom@users.noreply.github.com>
2017-11-14 18:50:54 +0100

    Aggiornata la funzione "deletePunctuation".
    Aggiornata la funzione che rimuove la punteggiatura, il resto del codice non è strettamente necessario.

--- Hash/FileHash.cpp
+++ Hash/FileHash.cpp
@@ -5,6 +5,9 @@
 #include<bitset>
 
 using namespace std;
+static int fileLength;
+
+
 
 unsigned long hash(const char *str)
 {
@@ -17,7 +20,17 @@ unsigned long hash(const char *str)
     return hash;
 }
 
-void removePunctuation(char *& text);//DA IMPLEMENTARE!!!!!
+char* deletePunctuation (char* text)
+
+  {
+    char* result = new char [fileLength];
+    for (int i = 0; i < fileLength; i++)
+    {
+      if (!(((text[i]>64)&&(text[i]<91))||((text[i]>96)&&(text[i]<123)))) result[i] = char(32);
+      else result[i] = text[i];
+    }
+    return result;
+  }
 
 int main (int argc, char* argv[])
 {
@@ -29,7 +42,6 @@ int main (int argc, char* argv[])
   char* reservoir;
   string word;
   char* wordBuffer;
-  int fileLength;
 
   //Stabiliamo quanto è lungo il file, e salviamo tale lunghezza in fileLength.
   fileInput.seekg(0, ios::end);
@@ -43,8 +55,9 @@ int main (int argc, char* argv[])
   //Chiudiamo lo stream di lettura da file.
   fileInput.close();
   //Inizializziamo lo stream con il buffer.
-  memoryStream.str(reservoir);
+  memoryStream.str(deletePunctuation(reservoir));
   memoryStream.seekg(0, ios::beg);
+  //Rimuoviamo la punteggiatura
   int streamPosition = 0;
 
   /*
@@ -55,6 +68,7 @@ int main (int argc, char* argv[])
   while(1)
   {
     memoryStream>>word;
+    cout<<word;
     if(memoryStream.fail()) break;
     fileOutput<<bitset<64>(hash(word.data()))<<endl;
   }
