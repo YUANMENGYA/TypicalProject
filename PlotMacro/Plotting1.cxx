2a38757bf13cc2f8281056f1b0664285837085a2
EleMisi <33552669+EleMisi@users.noreply.github.com>
2017-12-04 15:27:31 +0100

    Aggiunta Plotting Macro

---
+++ PlotMacro/Plotting1.cxx
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include "TRandom.h"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TGraph.h"
+
+using namespace std;
+
+void fill_vector(vector < pair<string, double> > &v);
+
+
+
+
+void fill_vector(vector < pair<string, double> > &v) {
+
+	double x;
+	string name;
+
+	for (int i = 1; i < 1000; ++i)
+	{
+		name = "banana";
+		x = exp(-double(i)/1000);
+		v.push_back(pair<string, double>(name, x));
+
+	}
+	
+}
+
+
+void plot()
+
+{
+	vector <pair <string, double> > v;
+        fill_vector(v);
+
+        int n=v.size();
+        double y[n], x[n];
+  
+      //Controllo elementi vector
+	for (int i = 0; i < v.size(); ++i)
+	{
+	   cout << "[posto " << i << " del primo] =" << v[i].first << endl;
+	   cout << "[posto " << i << " del secondo] =" << v[i].second << endl;
+
+	}
+
+      //Preparazione elementi Graph
+       for (int i=0;i<v.size();i++)
+       {
+          x[i] = i;
+          y[i] = v[i].second;
+        }
+
+  
+       //Graph & cosmetics
+        TGraph* gr = new TGraph(n,x,y);
+	TCanvas *c1 = new TCanvas("c1", "PlotCanvas", 200, 10, 600, 400);
+
+	c1->cd();
+        gr->SetLineColor(kRed);
+	gr->Draw("AC");
+	c1->Print("PlotMacro.gif");
+	c1->Print("PlotMacro.C");
+
+}
+
